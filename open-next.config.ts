97845e1d9e3eeb6f3a9490ad5454a1b0c4449c36
luisfernandosoni <soniglf@gmail.com>
2026-01-29 13:07:09 -0600

    fix bundling

--- open-next.config.ts
+++ open-next.config.ts
@@ -3,6 +3,6 @@ import { defineCloudflareConfig } from '@opennextjs/cloudflare/config'
 
 export default defineCloudflareConfig({
   build: {
-    external: ['typescript', 'drizzle-kit', 'sharp'],
+    external: ['typescript', 'drizzle-kit', 'sharp', 'sqlite', '@payloadcms/db-d1-sqlite'],
   },
 } as any)
