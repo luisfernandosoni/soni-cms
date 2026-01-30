ced9a2e2b1eb77e2911cf3e46ecc4d5c79948368
luisfernandosoni <soniglf@gmail.com>
2026-01-29 23:21:06 -0600

    perf: Externalize `graphql` in Cloudflare configuration and explicitly run Next.js build during application deployment.

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,8 +1,7 @@
-// default open-next.config.ts file created by @opennextjs/cloudflare
 import { defineCloudflareConfig } from '@opennextjs/cloudflare/config'
 
 export default defineCloudflareConfig({
   build: {
-    external: ['typescript', 'drizzle-kit', 'drizzle-kit/api', 'sharp'],
+    external: ['typescript', 'drizzle-kit', 'drizzle-kit/api', 'sharp', 'graphql'],
   },
 } as any)
